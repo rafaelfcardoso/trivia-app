eea061d2a315ea25f2273ee7deda4af79a0f9234
gustavoweiss <gustavoweissformiga@gmail.com>
2022-03-09 17:00:39 -0300

    dev-created login page and handleChange

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class Login extends React.Component {
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <section>
-        <label htmlFor="playerName">
-          <input type="text" id="playerName" name="name" placeholder="Digite seu nome:" />
-        </label>
+        <form>
+          <label htmlFor="playerEmail">
+            <input
+              type="email"
+              id="playerEmail"
+              name="gravatarEmail"
+              placeholder="Digite seu email:"
+              data-testid="input-gravatar-email"
+            />
+          </label>
+          <label htmlFor="playerName">
+            <input
+              type="text"
+              id="playerName"
+              name="name"
+              placeholder="Digite seu nome:"
+              data-testid="input-player-name"
+            />
+          </label>
+          <button data-testid="btn-play" id="submitBtn" type="submit">Jogar!</button>
+        </form>
       </section>
     );
   }
