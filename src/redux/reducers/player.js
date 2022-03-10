df60cf3d011781de51b09fb1cabf284b3a8b7c00
gustavoweiss <gustavoweissformiga@gmail.com>
2022-03-10 08:24:43 -0300

    fix: changed names and import on reducers

--- src/redux/reducers/player.js
+++ src/redux/reducers/player.js
@@ -1,11 +1,11 @@
-const INNITIAL_STATE = {
+const INITIAL_STATE = {
   name: '',
   assertions: '',
   score: 0,
   gravatarEmail: '',
 };
 
-const playerReducer = (state = INNITIAL_STATE, action) => {
+const playerReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
   case '':
   default:
