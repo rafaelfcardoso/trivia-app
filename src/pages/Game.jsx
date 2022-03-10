382d060c3c0410b790cb3a2e146373f08850e4b3
Rafael <rafael.fcardoso@hotmail.com>
2022-03-10 19:09:11 -0300

    dev: implements map functions to render questions on game component

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Boolean from '../components/Booleans';
 import Header from '../components/Header';
 import Multiple from '../components/Multiple';
@@ -6,6 +7,7 @@ import '../css/Game.css';
 
 class Game extends React.Component {
   render() {
+    const { questions } = this.props;
     return (
       <div>
         <header>
@@ -17,12 +19,33 @@ class Game extends React.Component {
             <h3>Categoria</h3>
             <p>Pergunta</p>
           </div>
-          <Multiple />
-          <Boolean />
+          {
+            questions.map((question) => {
+              if (question.type === 'multiple') {
+                return (<Multiple
+                  correctAnswer={ question.correct_answer }
+                  incorrectAnswers={ question.incorrect_answers }
+                />);
+              }
+              return (<Boolean
+                correctAnswer={ question.correct_answer }
+                incorrectAnswers={ question.incorrect_answers }
+                key={ Math.random() }
+              />
+              );
+            })
+          }
+
         </div>
       </div>
     );
   }
 }
 
-export default Game;
+const mapStateToProps = (state) => (
+  {
+    questions: state.questions,
+  }
+);
+
+export default connect(mapStateToProps, null)(Game);
