95905b00635521c728e265c0d160d26a1f1af6af
Rafael <rafael.fcardoso@hotmail.com>
2022-03-15 21:01:03 -0300

    dev- style for Ranking Page

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import md5 from 'crypto-js/md5';
 import { getGames } from '../helpers/localStorage';
 import { resetPlayer, resetQuestionIndex } from '../redux/actions';
+import '../css/Ranking.css';
 // import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
@@ -31,25 +32,35 @@ class Ranking extends React.Component {
   render() {
     const { games } = this.state;
     return (
-      <div>
-        <h1 data-testid="ranking-title">Ranking</h1>
-        <button
-          type="button"
-          data-testid="btn-go-home"
-          onClick={ this.handleHomeBtn }
-        >
-          Início
-        </button>
-        {
-          games.map(({ name, score, gravatarEmail }, index) => (
-            <div key={ Math.random() }>
-              <span data-testid={ `player-name-${index}` }>{name}</span>
-              <img src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail.trim().toLowerCase()).toString()}` } alt="" />
-              <span data-testid={ `player-score-${index}` }>{score}</span>
-            </div>
-          ))
-        }
-      </div>
+      <section className="ranking-page-content">
+        <h1>Ranking</h1>
+        <div className="ranking-form">
+          <div className="ranking-container">
+            <button
+              type="button"
+              data-testid="btn-go-home"
+              onClick={ this.handleHomeBtn }
+            >
+              Início
+            </button>
+            {
+              games.map(({ name, score, gravatarEmail }, index) => (
+                <div key={ Math.random() } className="container">
+                  <img src={ `https://www.gravatar.com/avatar/${md5(gravatarEmail.trim().toLowerCase()).toString()}` } alt="" />
+                  <div className="container-text">
+                    <p data-testid={ `player-name-${index}` }>{name}</p>
+                    <p data-testid={ `player-score-${index}` }>
+                      Pontuação:
+                      { score }
+                    </p>
+                  </div>
+                  <br />
+                </div>
+              ))
+            }
+          </div>
+        </div>
+      </section>
     );
   }
 }
